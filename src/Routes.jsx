f6cc66dbf24959345577cd1169e152afe3f9970d
Luka Butikashvili <lukabutikashvili@gmail.com>
2022-01-18 17:11:45 +0400

    feat: add routes

---
+++ src/Routes.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import routes from "./config/routes";
+import { Route, Routes as ReactRoutes } from "react-router-dom";
+
+import MainPage from "./pages/Main";
+import ProductPage from "./pages/Product";
+import UsersPage from "./pages/Users";
+import UserDetailedPage from "./pages/UserDetailed";
+import SavedPage from "./pages/Saved";
+import CheckoutPage from "./pages/Checkout";
+
+const Routes = () => {
+  return (
+    <ReactRoutes>
+      <Route path={routes.MAIN_PAGE} element={<MainPage />} />
+      <Route path={routes.PRODUCT_PAGE} element={<ProductPage />} />
+      <Route path={routes.USERS_PAGE} element={<UsersPage />} />
+      <Route path={routes.USER_DETAILED_PAGE} element={<UserDetailedPage />} />
+      <Route path={routes.SAVED_PAGE} element={<SavedPage />} />
+      <Route path={routes.CHECKOUT} element={<CheckoutPage />} />
+    </ReactRoutes>
+  );
+};
+
+export default Routes;
---
+++ src/config/routes.jsx
@@ -0,0 +1,10 @@
+const routes = {
+  MAIN_PAGE: "/",
+  USERS_PAGE: "/users",
+  USER_DETAILED_PAGE: "/user/:id",
+  PRODUCT_PAGE: "/product/:id",
+  SAVED_PAGE: "/saved",
+  CHECKOUT: "/checkout",
+};
+
+export default routes;
