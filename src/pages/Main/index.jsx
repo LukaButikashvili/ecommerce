9346e599e31a6e02b39d9a40a7df7d50a59f19c8
Luka Butikashvili <lukabutikashvili@gmail.com>
2022-01-21 16:34:24 +0400

    feat: add a spinner

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,25 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import Carusel from "../../components/Carusel/Carusel";
+import { useDispatch, useSelector } from "react-redux";
+import BarLoader from "react-spinners/BarLoader";
+import { css } from "@emotion/react";
+
 import Header from "../../components/Header/Header";
+import Carusel from "../../components/Carusel/Carusel";
 import ProductList from "../../components/ProductList/ProductList";
 import { getProductsAction } from "../../redux/product/actions/productActions";
+import statuses from "../../config/statuses";
+
+const override = css`
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%; -50%);
+`;
 
 const MainPage = () => {
   const dispatch = useDispatch();
+  const { status } = useSelector((state) => state.productReducer);
 
   useEffect(() => {
     dispatch(getProductsAction());
@@ -16,8 +29,15 @@ const MainPage = () => {
   return (
     <div>
       <Header />
-      <Carusel />
-      <ProductList />
+      {(status === statuses.INITIAL || status === statuses.PENDING) && (
+        <BarLoader css={override} size={150} />
+      )}
+      {status === statuses.SUCCESS && (
+        <>
+          <Carusel />
+          <ProductList />
+        </>
+      )}
     </div>
   );
 };
